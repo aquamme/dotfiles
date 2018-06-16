48412b139bcdc3e081b3dae6f0f4be405cfb0189
Aaron Quamme <aaron.quamme@gmail.com>
2018-06-16 13:25:31 -0500

    Update install script to symlink i3 config

--- install.sh
+++ install.sh
@@ -3,7 +3,9 @@
 ln -s ~/dotfiles/.vimrc ~/.vimrc 
 ln -s ~/dotfiles/.vim ~/.vim
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/.config/i3/config ~/.config/i3/config
 
 cd ~/dotfiles/.vim/
 git clone https://github.com/ctrlpvim/ctrlp.vim.git bundle/ctrlp.vim
 git clone git://github.com/tpope/vim-vinegar.git bundle/vim-vinegar.git
+
