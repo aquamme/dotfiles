e5681a845c930c9fa8566fb7bf445275b369db14
Aaron Quamme <aaron.quamme@gmail.com>
2018-08-09 22:59:05 -0500

    fish prompt git info: workaround for chars that won't print :(

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -10,7 +10,7 @@ set gray (set_color -o black)
 
 # Fish git prompt
 set __fish_git_prompt_showdirtystate 'yes'
-set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showstashstate 'no'
 set __fish_git_prompt_showuntrackedfiles 'yes'
 set __fish_git_prompt_showupstream 'yes'
 set __fish_git_prompt_color_branch yellow
@@ -18,10 +18,10 @@ set __fish_git_prompt_color_upstream_ahead green
 set __fish_git_prompt_color_upstream_behind red
 
 # Status Chars
-set __fish_git_prompt_char_dirtystate '⚡'
-set __fish_git_prompt_char_stagedstate '→'
-set __fish_git_prompt_char_untrackedfiles '☡'
-set __fish_git_prompt_char_stashstate '↩'
+set __fish_git_prompt_char_dirtystate 'd'
+set __fish_git_prompt_char_stagedstate 's'
+set __fish_git_prompt_char_untrackedfiles 'u'
+set __fish_git_prompt_char_stashstate '_'
 set __fish_git_prompt_char_upstream_ahead '+'
 set __fish_git_prompt_char_upstream_behind '-'
 
